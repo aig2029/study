53f0f9707329ace8ef03e3830e63ae94bcc57639
이승진 <aig2029@naver.com>
2019-09-14 02:35:23 +0900

    자바 기초 프로그래밍 강좌 7강 - 기본 입출력(Input & Output) (Java Programming Tutorial 2017 #7)

--- src/study/Study190914_0.java
+++ src/study/Study190914_0.java
@@ -4,6 +4,15 @@ public class Study190914_0 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int count = 0;
+		
+		for (;;) {
+			System.out.println("출력");
+			count++;
+			if(count==10) {
+				break;
+			}
+		}
 		
 	}
 
