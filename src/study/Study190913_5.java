4f3c322cda1a9ca5295f38037ffeca9656ced059
이승진 <aig2029@naver.com>
2019-09-13 20:57:17 +0900

    자바 기초 프로그래밍 강좌 5강 - 조건문 & 반복문 ① (Java Programming Tutorial 2017 #5)

---
+++ src/study/Study190913_5.java
@@ -0,0 +1,13 @@
+package study;
+
+public class Study190913_5 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 20;
+		i+=1;
+		System.out.println((100<i) || (i<200));
+		
+	}
+
+}
---
+++ src/study/Study190913_6.java
@@ -0,0 +1,18 @@
+package study;
+
+public class Study190913_6 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "I Love You.";
+		if(a.contains("Love")) {
+			//포함하는 경우 실행되는 부문
+			System.out.println("Me Too.");
+		}
+		else {
+			//포함하지 않은 경우 실행되는 부분
+			System.out.println("I Hate You.");
+		}
+	}
+
+}
