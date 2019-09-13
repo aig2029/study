f2b484c781b4f40b54e267728a3af95ec16d1f97
이승진 <aig2029@naver.com>
2019-09-13 20:19:21 +0900

    study0819

--- src/study/Study190913_0.java
+++ src/study/Study190913_0.java
@@ -2,8 +2,6 @@ package study;
 
 public class Study190913_0 {
 	
-	
-	// 공부 기본.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
---
+++ src/study/Study190913_1.java
@@ -0,0 +1,16 @@
+package study;
+
+public class Study190913_1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println("현재의 a는 " + a + "입니다.");
+		a++;
+		System.out.println("현재의 a는 " + a + "입니다.");
+		System.out.println("현재의 a는 " + ++a + "입니다.");
+		System.out.println("현재의 a는 " + a++ + "입니다.");
+		System.out.println("현재의 a는 " + a + "입니다.");
+	}
+
+}
