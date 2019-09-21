564be409a5c5569960760f6e48b440fad6fc4d43
이승진 <aig2029@naver.com>
2019-09-21 09:33:40 +0900

    edited

--- src/study/Study190921_0.java
+++ src/study/Study190921_0.java
@@ -4,15 +4,6 @@ public class Study190921_0 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("이승진");
-		System.out.println(10+10);
-		
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
 		
 	}
 
---
+++ src/study/Study190921_1.java
@@ -0,0 +1,19 @@
+package study;
+
+public class Study190921_1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이승진");
+		System.out.println(10+10);
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
